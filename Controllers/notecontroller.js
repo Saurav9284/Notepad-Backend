acd070d785b0120e4b92ea6af38240783261456f
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-02-02 12:21:40 +0530

    Get request added (Private)

--- Controllers/notecontroller.js
+++ Controllers/notecontroller.js
@@ -1,14 +1,26 @@
 const express = require('express')
 const NoteModel = require('../Models/Notes')
+
 const noteController = express.Router()
 
+// get
 
+noteController.get('/', async (req, res) => {
+    try {
+        const userId = req.userId;
+        const userNotes = await NoteModel.find({ createrId: userId });
+        res.json(userNotes);
+    } catch (error) {
+        console.log(error);
+        res.send({ msg: 'Something went wrong' });
+    }
+});
 
 // Create 
+
 noteController.post('/create', async (req,res)=>{
     const userId = req.userId
     const {title, description} = req.body;
-    
     if(!title || !description){
         return res.send({msg:'Please fill all the details'})
     }
